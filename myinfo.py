210f32ca7f637f65a6eaef0782c6461b78d717df
KevinGemian <146225525+KevinGemian@users.noreply.github.com>
2024-01-18 12:56:36 -0500

    my information v1

---
+++ myinfo.py
@@ -0,0 +1,6 @@
+# CS175L
+#Kevin Gemian
+#myInfo
+print ('My name is: Kevin Gemian')
+print ('My address  is: 400 Cedar Ave, Long Branch, New Jersey, 07740')
+print('My phone number is: 732-123-1223')
