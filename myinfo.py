519ee9ef6e597a5af25b5f07f28113ed3153c127
KevinGemian <146225525+KevinGemian@users.noreply.github.com>
2024-01-20 01:31:04 -0500

    My information v2

--- myinfo.py
+++ myinfo.py
@@ -4,3 +4,4 @@
 print ('My name is: Kevin Gemian')
 print ('My address  is: 400 Cedar Ave, Long Branch, New Jersey, 07740')
 print('My phone number is: 732-123-1223')
+print ('My College Major: Software Engineering')
