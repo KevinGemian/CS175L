241b12f4aa1de8a8b191ba8cab6daf8dbb029c0c
KevinGemian <146225525+KevinGemian@users.noreply.github.com>
2024-02-12 12:27:10 -0500

    restaurant V2

--- restaurant.py
+++ restaurant.py
@@ -1,42 +1,48 @@
 #Kevin Gemian
 #CS 175L
-#restaurant
-
-vegetarian = str(input('Is anyone in your party vegetarian?(Yes or No): '))
-vegan = str(input('Is anyone in your party vegan?(Yes or No): '))
-glutenFree = str(input('Is anyone in your party gluten-free?(Yes or No): '))
-vegetarian =  vegetarian.lower()
-vegan = vegan.lower()
-glutenFree = glutenFree.lower()
-print ('Here are your restaurant choices:')
-if vegetarian != 'yes' and vegan != 'yes' and glutenFree != 'yes':
-    print("Joe's Gourmet Burgers")
-    print("Main Street Pizza Company")
-    print("Corner Cafè")
-    print ("Mama's Fine Italian")
-    print ("The Chef's Kitchen")
-if vegetarian == 'yes' and vegan != 'yes' and glutenFree != 'yes':
-    print ("Joe's Gourmet Burgers")
-    print ("Main Street Pizza Company")
-    print("Corner Cafè")
-    print("The Chef's Kitchen")
-if vegetarian != 'yes' and vegan != 'yes' and glutenFree == 'yes':
-    print ("Main street Pizza Company")
-    print ("Corner Cafè")
-    print ("Chef's Kitchen")
-if vegetarian == 'yes' and vegan != 'yes' and glutenFree == 'yes':
-    print ("Main street Pizza Company")
-    print ("Corner Cafè")
-    print ("Chef's Kitchen")
-if vegetarian != 'yes' and vegan == 'yes' and glutenFree != 'yes':
-    print ("Corner Cafè")
-    print ("Chef's Kitchen")
-if vegetarian != 'yes' and vegan == 'yes' and glutenFree == 'yes':
-    print ("Corner Cafè")
-    print ("Chef's Kitchen")
-if vegetarian == 'yes' and vegan == 'yes' and glutenFree != 'yes':
-    print ("Corner Cafè")
-    print ("Chef's Kitchen")
-if vegetarian == 'yes' and vegan == 'yes' and glutenFree == 'yes':
-    print ("Corner Cafè")
-    print ("Chef's Kitchen")
+#restaurant V2
+loop = 'xyz'
+while loop != 'no':
+    vegetarian = str(input('Is anyone in your party vegetarian?(Yes or No): '))
+    vegan = str(input('Is anyone in your party vegan?(Yes or No): '))
+    glutenFree = str(input('Is anyone in your party gluten-free?(Yes or No): '))
+    vegetarian =  vegetarian.lower()
+    vegan = vegan.lower()
+    glutenFree = glutenFree.lower()
+    print ('Here are your restaurant choices:')
+    if vegetarian != 'yes' and vegan != 'yes' and glutenFree != 'yes':
+        print("Joe's Gourmet Burgers")
+        print("Main Street Pizza Company")
+        print("Corner Cafè")
+        print ("Mama's Fine Italian")
+        print ("The Chef's Kitchen")
+    if vegetarian == 'yes' and vegan != 'yes' and glutenFree != 'yes':
+        print ("Joe's Gourmet Burgers")
+        print ("Main Street Pizza Company")
+        print("Corner Cafè")
+        print("The Chef's Kitchen")
+    if vegetarian != 'yes' and vegan != 'yes' and glutenFree == 'yes':
+        print ("Main street Pizza Company")
+        print ("Corner Cafè")
+        print ("Chef's Kitchen")
+    if vegetarian == 'yes' and vegan != 'yes' and glutenFree == 'yes':
+        print ("Main street Pizza Company")
+        print ("Corner Cafè")
+        print ("Chef's Kitchen")
+    if vegetarian != 'yes' and vegan == 'yes' and glutenFree != 'yes':
+        print ("Corner Cafè")
+        print ("Chef's Kitchen")
+    if vegetarian != 'yes' and vegan == 'yes' and glutenFree == 'yes':
+        print ("Corner Cafè")
+        print ("Chef's Kitchen")
+    if vegetarian == 'yes' and vegan == 'yes' and glutenFree != 'yes':
+        print ("Corner Cafè")
+        print ("Chef's Kitchen")
+    if vegetarian == 'yes' and vegan == 'yes' and glutenFree == 'yes':
+        print ("Corner Cafè")
+        print ("Chef's Kitchen")
+    loop = input('Enter \'yes\' if you would like to do another restaurant search (enter \'no\' to end):')
+    loop = loop.lower()
+    if loop == 'no':
+        print('End of Program!')
+        break
